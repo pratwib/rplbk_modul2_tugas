a994247af17f6ba1b020c00932e117287ab490eb
nyxx-bit <alkhawarismi989@gmail.com>
2022-10-24 05:23:14 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -28,5 +28,52 @@ export default function App() {
         searchResults.map((item) => (
           <div key={item.nim[0]} className="results">
             <h5>JrengJreng! Ini Dia Datanya : </h5>
+            <div className="profile-card">
+              <div className="card-header">
+                <h3>{item.nama_lengkap}</h3>
+                <div className="img-profile">
+                  <img src="https://img2.pngdownload.id/20180522/wll/kisspng-logo-black-white-font-apropoe-s-5b03bdd117fc68.8758133115269718570983.jpg"></img>
+                </div>
+              </div>
 
+              <div className="card-body">
+                <table border={0}>
+                  <tr>
+                    <td>Nama Panggilan</td>
+                    <td>:</td>
+                    <td>{item.nama_panggilan}</td>
+                  </tr>
+                  <tr>
+                    <td>NIM</td>
+                    <td>:</td>
+                    <td>{item.nim}</td>
+                  </tr>
+                  <tr>
+                    <td>No. Handphone</td>
+                    <td>:</td>
+                    <td>{item.nomor_telepon}</td>
+                  </tr>
+                  <tr>
+                    <td>Hobi</td>
+                    <td>:</td>
+                    <td>{item.hobi}</td>
+                  </tr>
+                </table>
+              </div>
 
+              <div className="card-footer">
+                <div className="medsos">
+                  <img src="https://i.pinimg.com/originals/1b/3b/ec/1b3bec070f7bb1c007dbaaeafaeb21b7.png"></img>
+                  <p> {item.email}</p>
+                </div>
+                <div className="medsos">
+                  <img src="https://cdn.iconscout.com/icon/free/png-256/line-messenger-675693.png"></img>
+                  <p> {item.id_line}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+    </div>
+  );
+}
